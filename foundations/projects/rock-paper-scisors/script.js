203a1ac50f65e83e0bc1dfc93cdddc2beb4aa647
szy251 <szy251@wp.pl>
2025-11-14 11:41:29 +0100

    Add function to play few rounds

--- foundations/projects/rock-paper-scisors/script.js
+++ foundations/projects/rock-paper-scisors/script.js
@@ -26,28 +26,28 @@ function playRound(humanChoice, computerChoice){
     switch(humanChoice){
         case 'rock':
             if(computerChoice === 'paper'){
-                console.log("You lose! Paper beats rock");
+                console.log('You lose! Paper beats rock');
                 computerScore +=1;
             } else{
-                console.log("You win! Rock beats scisors");
+                console.log('You win! Rock beats scisors');
                 humanScore +=1;
             }
             return;
         case 'paper':
             if(computerChoice === 'scisors'){
-                console.log("You lose! Scisors beats paper");
+                console.log('You lose! Scisors beats paper');
                 computerScore +=1;
             } else{
-                console.log("You win! Paper beats rock");
+                console.log('You win! Paper beats rock');
                 humanScore +=1;
             }
             return;
         case 'scisors':
             if(computerChoice === 'rock'){
-                console.log("You lose! Rock beats scisors");
+                console.log('You lose! Rock beats scisors');
                 computerScore +=1;
             } else{
-                console.log("You win! Scisors beats paper");
+                console.log('You win! Scisors beats paper');
                 humanScore +=1;
             }
             return;
@@ -56,5 +56,25 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
+function playGame(){
+    humanScore = 0;
+    computerScore = 0;
+    for(i = 1; i <= 5; i++){
+        console.log('Roound number' + i);
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    if(humanScore > computerScore){
+        console.log('You won game! Your score was ' + humanScore + '. Computer scroe was ' + computerScore +'.');
+    } else if(humanScore < computerScore){
+        console.log('You lost game! Your score was ' + humanScore + '. Computer scroe was ' + computerScore +'.');
+    } else{
+        console.log('Remis! Your and computer score was ' + humanScore + '.');
+    }
+}
 let humanScore = 0;
 let computerScore = 0;
+
+
+playGame()
