337ab208d57f6ef2d21ce2f38d5d4f7451470708
szy251 <szy251@wp.pl>
2025-11-14 14:13:14 +0100

    Format script file

--- foundations/projects/rock-paper-scisors/script.js
+++ foundations/projects/rock-paper-scisors/script.js
@@ -1,7 +1,7 @@
-function getComputerChoice(){
-    let choice = Math.floor(Math.random() *3);
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
 
-    switch(choice){
+    switch (choice) {
         case 0:
             return 'rock';
         case 1:
@@ -13,42 +13,42 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     return prompt('Chose between rook, paper, scisors').toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice){
-    if(humanChoice == computerChoice){
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
         console.log('Remis! Both have ' + humanChoice);
         return;
     }
 
-    switch(humanChoice){
+    switch (humanChoice) {
         case 'rock':
-            if(computerChoice === 'paper'){
+            if (computerChoice === 'paper') {
                 console.log('You lose! Paper beats rock');
-                computerScore +=1;
-            } else{
+                computerScore += 1;
+            } else {
                 console.log('You win! Rock beats scisors');
-                humanScore +=1;
+                humanScore += 1;
             }
             return;
         case 'paper':
-            if(computerChoice === 'scisors'){
+            if (computerChoice === 'scisors') {
                 console.log('You lose! Scisors beats paper');
-                computerScore +=1;
-            } else{
+                computerScore += 1;
+            } else {
                 console.log('You win! Paper beats rock');
-                humanScore +=1;
+                humanScore += 1;
             }
             return;
         case 'scisors':
-            if(computerChoice === 'rock'){
+            if (computerChoice === 'rock') {
                 console.log('You lose! Rock beats scisors');
-                computerScore +=1;
-            } else{
+                computerScore += 1;
+            } else {
                 console.log('You win! Scisors beats paper');
-                humanScore +=1;
+                humanScore += 1;
             }
             return;
         default:
@@ -56,20 +56,20 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-function playGame(){
+function playGame() {
     humanScore = 0;
     computerScore = 0;
-    for(i = 1; i <= 5; i++){
-        console.log('Roound number' + i);
+    for (i = 1; i <= 5; i++) {
+        console.log('Roound number ' + i);
         const humanChoice = getHumanChoice();
         const computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice);
     }
-    if(humanScore > computerScore){
-        console.log('You won game! Your score was ' + humanScore + '. Computer scroe was ' + computerScore +'.');
-    } else if(humanScore < computerScore){
-        console.log('You lost game! Your score was ' + humanScore + '. Computer scroe was ' + computerScore +'.');
-    } else{
+    if (humanScore > computerScore) {
+        console.log('You won game! Your score was ' + humanScore + '. Computer scroe was ' + computerScore + '.');
+    } else if (humanScore < computerScore) {
+        console.log('You lost game! Your score was ' + humanScore + '. Computer scroe was ' + computerScore + '.');
+    } else {
         console.log('Remis! Your and computer score was ' + humanScore + '.');
     }
 }
@@ -77,4 +77,4 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-playGame()
+playGame();
