75b0eb58eed5cc847cc787b817fd92ea637ac554
szy251 <szy251@wp.pl>
2025-11-24 19:36:43 +0100

    Add button to change number of board blocks

--- foundations/projects/etch-a-sketch/script.js
+++ foundations/projects/etch-a-sketch/script.js
@@ -2,6 +2,7 @@ const BOARD_SIZE = 800;
 let currColor = "rgba(81, 215, 19, 1)";
 
 const board = document.querySelector(".board");
+const sizeBtn = document.querySelector(".size");
 
 board.addEventListener("mouseover", (event) => {
 	const currentBlock = event.target;
@@ -10,6 +11,25 @@ board.addEventListener("mouseover", (event) => {
 		currentBlock.style.backgroundColor = currColor;
 	}
 });
+
+sizeBtn.addEventListener("click", () => {
+	let size = prompt("Change number of blocks per side (max 100):");
+	if (size === null) return;
+	size = Number.parseInt(size);
+
+	if (isNaN(size) || size > 100 || size < 1) {
+		alert("Incorrect data");
+	} else {
+		clearBoard();
+		drawBoard(size, size);
+	}
+});
+
+function clearBoard() {
+	while (board.firstChild) {
+		board.removeChild(board.firstChild);
+	}
+}
 
 function drawBoard(lanes, blocks) {
 	for (let i = 0; i < lanes; i++) {
