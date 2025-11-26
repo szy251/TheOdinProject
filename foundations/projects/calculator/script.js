e90fea6c98ea4cd44901647a9b360b3d068f2d80
szy251 <szy251@wp.pl>
2025-11-26 10:59:46 +0100

    Add basic calculator functions

---
+++ foundations/projects/calculator/script.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+	return a + b;
+}
+function sub(a, b) {
+	return a - b;
+}
+function mlt(a, b) {
+	return a * b;
+}
+function div(a, b) {
+	if (b == 0) return "Error, can't devide by 0";
+	return a / b;
+}
