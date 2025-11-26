e9333a13b74788aa990e6badc7931c4a3ff7c51e
szy251 <szy251@wp.pl>
2025-11-26 11:08:16 +0100

    Add operate function

--- foundations/projects/calculator/script.js
+++ foundations/projects/calculator/script.js
@@ -8,6 +8,21 @@ function mlt(a, b) {
 	return a * b;
 }
 function div(a, b) {
-	if (b == 0) return "Error, can't devide by 0";
+	if (b == 0) return "Error, can't divide by 0";
 	return a / b;
+}
+
+function operate(a, b, sing) {
+	switch (sing) {
+		case "+":
+			return add(a, b);
+		case "-":
+			return sub(a, b);
+		case "*":
+			return mlt(a, b);
+		case "/":
+			return div(a, b);
+		default:
+			return "Error";
+	}
 }
