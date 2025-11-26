d19cb64a77f7c6761fe61a2b91a4990a30f2d782
szy251 <szy251@wp.pl>
2025-11-26 12:54:36 +0100

    Add logic to edit and display number after click

--- foundations/projects/calculator/script.js
+++ foundations/projects/calculator/script.js
@@ -1,3 +1,60 @@
+let dotUsed = false;
+let minusSing = false;
+
+const numBtns = document.querySelectorAll(".num");
+const displayCalculator = document.querySelector(".display");
+const singBtn = document.querySelector(".sing");
+const dotBtn = document.querySelector(".dot");
+const backBtn = document.querySelector(".backspace");
+
+numBtns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		let currNum = displayCalculator.textContent;
+		if (currNum === "-0") {
+			currNum = "-" + btn.textContent;
+		} else if (currNum === "0") {
+			currNum = btn.textContent;
+		} else {
+			currNum += btn.textContent;
+		}
+		displayCalculator.textContent = currNum;
+	});
+});
+
+singBtn.addEventListener("click", () => {
+	if (minusSing) {
+		displayCalculator.textContent = displayCalculator.textContent.slice(1);
+	} else {
+		displayCalculator.textContent = "-" + displayCalculator.textContent;
+	}
+	minusSing = !minusSing;
+});
+
+dotBtn.addEventListener("click", () => {
+	if (!dotUsed) {
+		displayCalculator.textContent = displayCalculator.textContent + ".";
+		dotUsed = true;
+	} else {
+		if (displayCalculator.textContent.at(-1) === ".") {
+			displayCalculator.textContent = displayCalculator.textContent.slice(0, -1);
+			dotUsed = false;
+		}
+	}
+});
+
+backBtn.addEventListener("click", () => {
+	let currNum = displayCalculator.textContent;
+	if (currNum.length === 1) {
+		currNum = "0";
+	} else if (currNum.length === 2 && minusSing) {
+		currNum = "-0";
+	} else {
+		if (currNum.slice(-1) === ".") dotUsed = false;
+		currNum = currNum.slice(0, -1);
+	}
+	displayCalculator.textContent = currNum;
+});
+
 function add(a, b) {
 	return a + b;
 }
@@ -12,8 +69,8 @@ function div(a, b) {
 	return a / b;
 }
 
-function operate(a, b, sing) {
-	switch (sing) {
+function operate(a, b, symbol) {
+	switch (symbol) {
 		case "+":
 			return add(a, b);
 		case "-":
